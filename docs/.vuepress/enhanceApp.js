355f7af7d666e1784f3d2747120c5359731a6819
jaydonhu <jaydonhu@tencent.com>
2021-07-16 13:49:33 +0800

    feat: 复制代码

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -2,28 +2,13 @@ import Doui from '@';
 import zhCN from 'ant-design-vue/lib/locale-provider/zh_CN';
 
 export default ({ Vue, options }) => {
-  Vue.use(Doui, {
-    vuex: {
-      saveKeys: ['vuex_common'],
-      initialState: {
-        vuex_loading: false,
-        vuex_common: {
-          language: 'zh-CN'
-        }
-      }
-    }
-  });
+  Vue.use(Doui);
 
   Vue.mixin({
-    provide: function () {
-      return {
-        localeData: {
-          antLocale: {
-            ...zhCN,
-            exist: true
-          }
-        }
-      };
-    }
+    provide: () => ({
+      localeData: {
+        antLocale: zhCN
+      }
+    })
   });
 };
